e5f60bbc0f0a441c216ab48421b134a26ac01d0f
sebastian <sgaviria@uniguajira.edu.co>
2020-09-16 21:10:31 -0500

    agrega calcular_nota

--- problema_1.py
+++ problema_1.py
@@ -7,9 +7,17 @@ class alumno():
     nota = float(input("digite la nota del alumno: "))
     print ("")
 
+    def calcular_nota(self):
+        if ((self.nota >= 3) and (self.nota <=5)):
+            print(f"el alumno {self.nombre} aprobo")
+        elif ((self.nota) < 3 and (self.nota >= 0)):
+            print(f"el alumno {self.nombre} reprobo")
+        else: 
+            print(f"nota no valida")
+
 def main():
     alumno1 = alumno()
-    
+    alumno1.calcular_nota()
 
     
 
