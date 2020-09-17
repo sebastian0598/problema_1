26b57920bd5e604081c45d2bc6a07d2ee2b7f036
sebastian <sgaviria@uniguajira.edu.co>
2020-09-16 19:24:49 -0500

    principio

---
+++ problema_1.py
@@ -0,0 +1,17 @@
+import os 
+class alumno():
+    os.system('cls')
+    print("ingresar nombre y nota de un alumno y decir si aprobo o no ")
+    print("")
+    nombre = input("digite el nombre del alumno: ")
+    nota = float(input("digite la nota del alumno: "))
+    print ("")
+
+def main():
+    alumno1 = alumno()
+    
+
+    
+
+if __name__ == "__main__":
+    main()
