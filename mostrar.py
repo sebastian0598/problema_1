288db778ab2130e4543d76d3cff5253005971cf2
carlosM99-MAX <cmgomezcotes@uniguajira.edu.co>
2020-09-17 00:40:20 -0500

    mostrar datos por carlos gomes

---
+++ mostrar.py
@@ -0,0 +1,10 @@
+    def mostrar_datos(self):
+        print(f"el nombre del alumno es: {self.nombre}")
+        print(f"su nota es: {self.nota}")
+        print("")
+def main():
+    alumno1.calcular_nota()
+
+
+if __name__ == "__main__":
+    main()
